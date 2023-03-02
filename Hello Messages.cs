6f681686bcff57f7756213ad4747073e5083416e
Derek Ward <125997543+dpw0525@users.noreply.github.com>
2023-03-02 10:52:57 -0600

    Add files via upload

---
+++ Hello Messages.cs
@@ -0,0 +1,28 @@
+namespace HelloMethods
+{
+    public class Message
+    {
+        public static string GetMessage(string lang)
+        {
+            if (lang.Equals("sp")) 
+            {
+                return "Hola Mundo";
+            }
+            else if (lang.Equals("fr"))
+            {
+                return "Bonjour le monde";
+            }
+            else if (lang.Equals("nl"))
+                {
+                return "hallo Wereld";
+                }
+
+            else
+
+            {
+                return "hello World";
+            }
+                
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+
+namespace HelloMethods
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            string message = Message.GetMessage("nl");
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
